db61ddb032f49ba226bf96f37ff6f98c13d25dba
Wilson Callisaya <demarktec@gmail.com>
2020-05-01 18:17:10 -0500

    fragmentos

---
+++ app/src/main/java/com/ingwilson/miparte2fragmento/EjemplosFragment.java
@@ -0,0 +1,26 @@
+package com.ingwilson.miparte2fragmento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ingwilson.miparte2fragmento.Ejemplo1.MainActivity;
+
+public class EjemplosFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mibotonera);
+    }
+
+    public void fragmentestatico(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void ejemplo2(View view) {
+        startActivity(new Intent(this, ListaCorreo.class));
+    }
+}
